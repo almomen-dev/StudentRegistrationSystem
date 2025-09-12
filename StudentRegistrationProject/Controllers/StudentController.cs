feb2a7df2e412e2040ea781f884fa96d7f198ec0
Al Momen <almomen.dev@gmail.com>
2025-09-12 22:36:06 +0600

    Added required things

---
+++ StudentRegistrationProject/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+using System.Collections.Generic;
+
+namespace YourNamespace.Controllers
+{
+    [Route("student")]
+    public class StudentController : Controller
+    {
+        // In-memory list to store students
+        private static List<Student> students = new List<Student>();
+
+        // GET: student/register
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: student/register
+        [HttpPost("register")]
+        public IActionResult Register(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                students.Add(student);
+                TempData["SuccessMessage"] = "Student registered successfully!";
+                return RedirectToAction("List");
+            }
+            TempData["ErrorMessage"] = "Please correct the errors!";
+            return View(student);
+        }
+
+        // GET: student/list
+        [Route("list")]
+        public IActionResult List()
+        {
+            return View(students);
+        }
+
+        // GET: student/json
+        [Route("json")]
+        public IActionResult GetJson()
+        {
+            return Json(students);
+        }
+    }
+}
---
+++ StudentRegistrationProject/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YourNamespace.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        [Range(16, 65, ErrorMessage = "Age must be between 16 and 65")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Course is required")]
+        public string Course { get; set; }
+    }
+}
